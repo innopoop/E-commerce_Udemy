c20fb987d5c25d69500ff3a9d60417d30b8474b8
Ashley Zhu <ashleyrawr74@gmail.com>
2020-07-03 16:49:18 -0700

    Added header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Shop from "./pages/shop/shop.component";
 import HomePage from "./pages/homepage/homepage.component";
+import Header from "./components/header/header-component";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 
@@ -11,6 +12,7 @@ function App() {
          It will render all of the Routes that have that path match
          Switch will make it so that ONLY ONE of the routes will be rendered and
          ONLY THE FIRST ONE*/}
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={Shop} />
