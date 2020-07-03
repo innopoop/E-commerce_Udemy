9dbb0f7ef5cfa03863adb4daf3b24cddb603e44c
Ashley Zhu <ashleyrawr74@gmail.com>
2020-07-03 16:29:21 -0700

    Added the shop collection preview

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
+import Shop from "./pages/shop/shop.component";
 import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 
 function App() {
-  const HatsPage = () => <h1>HATS PAGE</h1>;
   return (
     <div>
       {/*exact means you need exactly this ending of the url for it to route
@@ -13,7 +13,7 @@ function App() {
          ONLY THE FIRST ONE*/}
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/shop" component={Shop} />
       </Switch>
     </div>
   );
